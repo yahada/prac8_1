9ca7ad6386f0f64703494a69386d849e1220ca54
yahada <niadzy14@gmail.com>
2025-11-08 12:47:19 +0300

    program 1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,52 @@
+#include <iostream>
+
+int * copy (const int * v, size_t k, int * w) {
+  for (size_t i = 0; i < k; ++i) {
+    w[i] = v[i];
+  }
+  return w+k;
+}
+
+void destroy(int ** mtx, size_t created) {
+  for (size_t i = 0; i < created; i++) {
+    delete[] mtx[i];
+  }
+  delete[] mtx;
+}
+
+int ** ct(size_t n, size_t m) {
+  int ** mtx = new int * [n];
+  size_t created = 0;
+  try {
+    for (; created < n; ++created) {
+      mtx[created] = new int[m];
+    }
+  } catch (const std::bad_alloc & e) {
+    destroy(mtx, created);
+    throw;
+  }
+  return mtx;
+
+}
+
+int ** concat_rows(const int * const * a, size_t n1, size_t m1, const int * const * b, size_t n2, size_t m2) {
+  size_t n = n1 > n2 ? n2 : n1;
+  size_t m = m1 + m2;
+  int ** c = ct(n, m);
+  for (size_t i = 0; i < n; ++i) {
+    const int * row_a = a[i];
+    const int * row_b = b[i];
+    int * row_c = c[i];
+    row_c  = copy(row_a, m1, row_c);
+    copy(row_b, m2, row_c);
+  }
+  return c;
+}
+
+
+
+
+
 int main () {
 
 }
